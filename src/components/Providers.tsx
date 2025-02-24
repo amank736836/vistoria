de3028572c41ac43d7ac10812280c7c445eb612d
Aman Kumar <amankarguwal0@gmail.com>
2025-02-24 22:04:49 +0530

    Add Providers component for ImageKit and NextAuth integration

---
+++ src/components/Providers.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { ImageKitProvider } from "imagekitio-next";
+import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
+
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
+const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
+
+export default function Providers({ children }: { children: ReactNode }) {
+  const authenticator = async () => {
+    try {
+      const response = await fetch("/api/imagekit-auth");
+
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(
+          `Request failed with status ${response.status}: ${errorText}`
+        );
+      }
+
+      const data = await response.json();
+      const { signature, expire, token } = data;
+      return { signature, expire, token };
+    } catch (error) {
+      const authError = error as Error;
+      throw new Error(
+        `Imagekit Authentication request failed: ${authError.message}`
+      );
+    }
+  };
+
+  return (
+    <SessionProvider>
+      <ImageKitProvider
+        urlEndpoint={urlEndpoint}
+        publicKey={publicKey}
+        authenticator={authenticator}
+      >
+        {children}
+      </ImageKitProvider>
+    </SessionProvider>
+  );
+}
